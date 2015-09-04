f47d7ce8cda8c2fc60cfeb6d1cd32510f6eb8caf
wenlei1 <wenlei1@WENLEI1-907ERQ5.lenovo.com>
2015-09-04 08:08:13 +0800

    my modify

--- src/com/wenlei/Test2.java
+++ src/com/wenlei/Test2.java
@@ -1,9 +1,6 @@
 package com.wenlei;
 
 public class Test2 {
-<<<<<<< HEAD
    private String s = "";
-=======
->>>>>>> branch 'develop' of https://github.com/wenleigithub/mp1.git
    private int i = 0;
 }
