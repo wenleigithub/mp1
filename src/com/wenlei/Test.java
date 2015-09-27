13d6572e71197318269d4545690534d9a1d89482
wenlei1 <wenlei1@lenovo.com>
2015-09-27 10:15:56 +0800

    d

--- src/com/wenlei/Test.java
+++ src/com/wenlei/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	
 	private void test(){
-		System.out.print(">>>>>>>>>>");
+		System.out.print(">>>>>>>>ffffffs>>");
 		System.out.print(">>>>>>>>>>");
 		System.out.print(">>>>>>>>>>");
 		//A first modify
